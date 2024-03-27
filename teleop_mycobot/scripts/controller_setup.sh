9e6049b476a4cb5de0b2fa554a9eb44b97726c02
Yukai ZHANG <yzhanghe@connect.ust.hk>
2024-03-27 14:58:43 +0800

    use relative path

--- teleop_mycobot/scripts/controller_setup.sh
+++ teleop_mycobot/scripts/controller_setup.sh
@@ -1,4 +1,6 @@
 #!/bin/zsh
+parent_path=$( cd "$(dirname "${(%):-%N}")" ; pwd -P )
+cd "$parent_path"
 source /opt/ros/noetic/setup.zsh
 source ../../../../devel/setup.zsh
 export ROS_MASTER_URI=http://10.4.176.116:11311

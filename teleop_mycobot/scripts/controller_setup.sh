744a9e0a594a98a01139aa7606b913f117e7e65a
Yukai ZHANG <yzhanghe@connect.ust.hk>
2024-03-28 14:33:43 +0800

    add go back to workspace

--- teleop_mycobot/scripts/controller_setup.sh
+++ teleop_mycobot/scripts/controller_setup.sh
@@ -3,5 +3,6 @@ parent_path=$( cd "$(dirname "${(%):-%N}")" ; pwd -P )
 cd "$parent_path"
 source /opt/ros/noetic/setup.zsh
 source ../../../../devel/setup.zsh
+cd "../../../../"
 export ROS_MASTER_URI=http://10.4.176.116:11311
 export ROS_HOSTNAME=10.4.176.116
